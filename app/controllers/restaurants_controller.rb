dbe682f24f65c31dffe6cd094d7daff9a31399de
Mun Kadell <kadellppsay@gmail.com>
2022-05-14 01:49:17 +0400

    41451

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,32 +1,46 @@
 class RestaurantsController < ApplicationController
-  before_action :set_restaurant, only: [:show]
-  def index
-    @restaurants = Restaurant.all
-  end
+  before_action :find_param, only: [:show, :edit, :update, :destroy]
 
-  def show
-  end
+ def index
+   @restaurants = Restaurant.all
+ end
 
-  def new
-    @restaurant = Restaurant.new
-  end
+ def show
+ end
 
-  def create
-    @restaurant = Restaurant.new(restaurant_params)
-    if @restaurant.save
-      redirect_to restaurant_path(@restaurant)
-    else
-      render :new
-    end
-  end
+ def new
+   @restaurant = Restaurant.new
+ end
 
-  private
+ def create
+   @restaurant = Restaurant.new(restaurant_params)
+   if @restaurant.save
+     redirect_to restaurants_path(@restaurant)
+   else
+     render :new
+   end
+ end
 
-  def set_restaurant
-    @restaurant = Restaurant.find(params[:id])
-  end
+ def update
+   @restaurant.update(restaurant_params)
+   redirect_to restaurant_path(@restaurant)
+ end
 
-  def restaurant_params
-    params.require(:restaurant).permit(:name, :address, :phone_number, :category)
-  end
+ def edit
+ end
+
+ def destroy
+   @restaurant.destroy
+   redirect_to restaurants_path
+ end
+
+ private
+
+ def restaurant_params
+   params.required(:restaurant).permit(:name, :address, :phone_number, :category)
+ end
+
+ def find_param
+   @restaurant = Restaurant.find(params[:id])
+ end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,22 +1,16 @@
-require 'faker'
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
 #
 # Examples:
+10.times do
+  Restaurant.create(
+    name: Faker::Restaurant.name,
+    address: Faker::Address.street_name,
+    category: ["chinese", "italian", "japanese", "french", "belgian"].sample
+  )
+end
+
+
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-puts "Cleaning database..."
-Restaurant.destroy_all
-
-puts "Creating restaurants..."
-
-5.times do |number|
-  phone_num = Faker::PhoneNumber.phone_number
-  puts phone_num
-  Restaurant.create!(name: Faker::Restaurant.name, address: Faker::Address.full_address,
-    phone_number: phone_num,
-     category: [ "chinese", "italian", "japanese", "french", "belgian" ].sample)
-  puts "Created #{Restaurant.count} new restaurants"
-end
-puts "Finished"
